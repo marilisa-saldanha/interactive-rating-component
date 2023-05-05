52a360176150d2f4169028be9aa903073cb821b0
Marilisa Saldanha <saldanhamarilisa@gmail.com>
2023-05-05 19:54:54 -0300

    update files

--- src/script.js
+++ src/script.js
@@ -1,18 +1,15 @@
 const result = document.getElementById("result")
-const btnSubmit = document.querySelector(".submit");
-const pageResult = document.querySelector(".card2");
+const btnSubmit = document.querySelector(".btnSubmit")
+const pageResult = document.querySelector(".card2")
+const form = document.querySelector(".formContainer")
 
-document.querySelectorAll(".btn").forEach(function (btn) {
-  btn.addEventListener("click", function () {
-    const value = btn.dataset.value
-    result.textContent = `${value}`
-  })
-})
-
-btnSubmit.addEventListener("click", () => {
+form.addEventListener("submit", (ev) => {
+  ev.preventDefault()
+  const rating = form.rating.value
+  result.textContent = `${rating}`
   if (result.textContent === "") {
     alert("Please, give us your feedback!")
   } else {
     pageResult.style.visibility = "visible"
-  } 
+  }
 })
